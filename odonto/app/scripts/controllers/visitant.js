f164e92bbdbd57504c9b3c1db33ddea3b45a40d7
damisgarcia <damisgarcia@gmail.com>
2015-10-13 14:00:52 -0300

    Add Logo

--- odonto/app/scripts/controllers/visitant.js
+++ odonto/app/scripts/controllers/visitant.js
@@ -15,4 +15,22 @@ angular.module('odontoApp')
       {image:"app/images/slide_02.jpg",text:"Lorem ipsum"},
       {image:"app/images/slide_03.jpg",text:"Lorem ipsum"}
     ]
+
+    this.services = [
+      {
+        image:"app/images/slide_01.jpg",
+        title:"Serviço #1",
+        desc:"These marketing boxes are a great place to put some information. These can contain summaries of what the company does, promotional information, or anything else that is relevant to the company. These will usually be below-the-fold."
+      },
+      {
+        image:"app/images/slide_02.jpg",
+        title:"Serviço #2",
+        desc:"These marketing boxes are a great place to put some information. These can contain summaries of what the company does, promotional information, or anything else that is relevant to the company. These will usually be below-the-fold."
+      },
+      {
+        image:"app/images/slide_03.jpg",
+        title:"Serviço #3",
+        desc:"These marketing boxes are a great place to put some information. These can contain summaries of what the company does, promotional information, or anything else that is relevant to the company. These will usually be below-the-fold."
+      }
+    ]
   });
